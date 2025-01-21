6462516253a3908035b984cea2317cc65d16276d
matheusfdosan <matheusfaustinoe20@gmail.com>
2025-01-20 21:03:57 -0300

    :bricks: ci: all right to start

---
+++ PostgresqlDB.js
@@ -0,0 +1,4 @@
+import { randomUUID } from "node:crypto"
+import sql from "./db.js"
+
+export default class DatabasePostgreSQL {}
--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 import fastify from "fastify"
+
 const server = fastify()
+
 
 
 server.listen({ port: process.env.PORT ?? 3333, host: "0.0.0.0" })
